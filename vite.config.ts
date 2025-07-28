7ab6db156a03e3c201485ee3fcfdf4f960a92fdf
Vitali <61989351+vitali007tut@users.noreply.github.com>
2025-07-28 20:08:14 +0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/my-vue-app/',
+  base: 'https://vitali007tut.github.io//my-vue-app/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
